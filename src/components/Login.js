bfb6e4cba7e95eb576ced598fb7ac5229722f3bb
budou114 <58349030+budou114@users.noreply.github.com>
2022-08-07 15:36:42 +0900

    Googleログイン機能を実装してみよう

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,19 @@
+import { signInWithPopup } from "firebase/auth"
+import { auth, provider } from "../firebase"
+
 const Login = () => {
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+
+    });
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
   )
 }
 
