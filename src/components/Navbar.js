c9ef373cfac324bc3d39bd13ab5d1a85ee714c09
budou114 <58349030+budou114@users.noreply.github.com>
2022-08-07 15:04:53 +0900

    ナビゲーションバーの雛形を作成しよう

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">ホーム</Link>
+      <Link to="/createPost">記事投稿</Link>
+      <Link to="/login">ログイン</Link>
+    </nav>
+  );
+}
+
+export default Navbar
