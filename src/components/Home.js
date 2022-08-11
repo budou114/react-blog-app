8bebf11fbbbc2e1886b60083ff1ce9947e6cc271
budou114 <58349030+budou114@users.noreply.github.com>
2022-08-11 15:02:10 +0900

    ホームコンポーネントの作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
+import "./Home.css";
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          aaaaaa
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3></h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
