4fd205be51f49830fcb23afd05a4b2aeedf4b283
budou114 <58349030+budou114@users.noreply.github.com>
2022-08-11 14:18:09 +0900

    記事投稿機能の作成

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,19 +1,44 @@
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+import { auth, db } from "../firebase";
 import "./CreatePost.css";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState();
+  const [postText, setPoseText] = useState();
+
+  const createPost = async () => {
+    await addDoc(collection(db, "posts"), {
+      title: title,
+      postsText: postText,
+      author: {
+        username: auth.currentUser.displayName,
+        id: auth.currentUser.uid
+      }
+    })
+  }
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div className="inputPost">
           <div>タイトル</div>
-          <input type="text" placeholder="タイトルを記入"/>
+          <input
+            type="text"
+            placeholder="タイトルを記入"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
             <div>投稿</div>
-            <textarea placeholder="投稿内容を記入"></textarea>
+            <textarea
+              placeholder="投稿内容を記入"
+            onChange={(e) => setPoseText(e.target.value)}
+
+            ></textarea>
         </div>
-        <button className="postButton">投稿する</button>
+        <button className="postButton" onClick={createPost}>投稿する</button>
       </div>
     </div>
   );
